39fbdda2a2ed2fd78fe3907751a5e0709752e379
xpwu <diodexp@foxmail.com>
2023-04-09 17:20:59 +0800

    Update xqueueTests.swift

--- xqueue/xqueueTests/xqueueTests.swift
+++ xqueue/xqueueTests/xqueueTests.swift
@@ -32,5 +32,96 @@ class xqueueTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+	
+	func testChannel1() async {
+		let expectation = self.expectation(description: "testChannel1")
+		
+		let ch = Channel<Int>(buffer: 0)
+		
+		Task {
+			let ret = await ch.Receive()
+			XCTAssertEqual(ret, 10)
+			expectation.fulfill()
+		}
+		
+		Task {
+			await ch.Send(10)
+		}
+		
+		self.wait(for: [expectation], timeout: 10)
+	}
+	
+	func testChannel2() async {
+		let expectation = self.expectation(description: "testChannel2")
+		
+		let ch = Channel<Int>(buffer: 0)
+		
+		Task {
+			await ch.Send(10)
+		}
+		
+		let ret = await ch.Receive()
+		XCTAssertEqual(ret, 10)
+		expectation.fulfill()
+		
+		self.wait(for: [expectation], timeout: 10)
+	}
+	
+	func testChannel3() async {
+		let expectation = self.expectation(description: "testChannel3")
+		
+		let ch = Channel<Int>(buffer: 0)
+		
+		Task {
+			let ret = await ch.Receive()
+			XCTAssertEqual(ret, 10)
+			expectation.fulfill()
+		}
+		
+		await ch.Send(10)
 
+		self.wait(for: [expectation], timeout: 10)
+	}
+	
+	func testChannelMore() async {
+		let expectation = self.expectation(description: "testChannelMore")
+		
+		let ch = Channel<Int>(buffer: .Unlimited)
+		
+		let count = 100
+		let sumA = sum()
+		
+		Task {
+			var sum = 0
+			for _ in 0..<count {
+				sum += await ch.Receive()
+			}
+			let a = await sumA.result()
+			
+			XCTAssertEqual(sum, a)
+			expectation.fulfill()
+		}
+		
+		for _ in 0..<count {
+			Task {
+				let v = Int.random(in: 0..<1000)
+				await sumA.add(v)
+				await ch.Send(v)
+			}
+		}
+		
+		self.wait(for: [expectation], timeout: 10)
+	}
+
+}
+
+actor sum {
+	var value = 0
+	func add(_ v: Int) {
+		value += v
+	}
+	
+	func result()->Int {
+		return value
+	}
 }
