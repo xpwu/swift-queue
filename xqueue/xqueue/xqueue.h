365f8413fbea90cbdc0268312b91130b43650928
xpwu <diodexp@foxmail.com>
2023-04-09 16:50:19 +0800

    init project

---
+++ xqueue/xqueue/xqueue.h
@@ -0,0 +1,18 @@
+//
+//  xqueue.h
+//  xqueue
+//
+//  Created by xpwu on 2023/4/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for xqueue.
+FOUNDATION_EXPORT double xqueueVersionNumber;
+
+//! Project version string for xqueue.
+FOUNDATION_EXPORT const unsigned char xqueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xqueue/PublicHeader.h>
+
+
